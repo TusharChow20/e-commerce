56a27e88e49c38a8f309e02a12d53cdea4f5628d
Tushar Chowdhury <tusharchowdhury20211@gmail.com>
2024-07-01 00:42:52 +0600

    hjh

--- abrar.ts
+++ abrar.ts
@@ -1,1 +1,9 @@
 console.log('abrar')
+
+
+// kjjkh
+console.log("Users before deletion:");
+console.log(userManager.getUsers());
+
+const isDeleted = userManager.deleteUser(1);
+console.log(`User deleted: ${isDeleted}`);
--- neamul.ts
+++ neamul.ts
@@ -1,1 +1,11 @@
 console.log('neamul')
+
+
+
+// hhkhjkh
+
+console.log("Users before deletion:");
+console.log(userManager.getUsers());
+
+const isDeleted = userManager.deleteUser(1);
+console.log(`User deleted: ${isDeleted}`);
